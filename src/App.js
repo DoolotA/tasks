57c43f1a4d825f9da804a6c37b4a7dd8a5805916
Doolot <doolotzhaparov@gmail.com>
2022-01-31 23:41:41 +0600

    task8

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Count from './components/Count';
+
+
+function App() {
+
+  return (
+    <>
+      
+      <Count />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Count.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Count () {
+  const [count, setCount ] = React.useState(0)
+
+
+  return (
+  <div>
+    <h1>{count}</h1>
+    <button onClick={() => setCount(count + 1)}>Increase</button>
+    <button onClick={() => setCount(count - 1)}>Decrease</button>
+    <button onClick={() => setCount(0)}>Reset</button>
+
+  </div>
+  );
+}
+
+export default Count;
