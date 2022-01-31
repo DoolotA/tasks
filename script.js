f9ae6c7a1cbfada5847a142638e1b7981b55b7a6
Doolot <doolotzhaparov@gmail.com>
2022-01-31 23:03:01 +0600

    task5

--- script.js
+++
@@ -1,8 +0,0 @@
-const todoList = document.getElementById('todo-list')
-let tasks = ['Buy lemonade', 'Make toasts', 'Repair car', 'Play games', 'Pet a cat'];
-for(let i = 0; i < tasks.length; i++) {
-  const li = document.createElement('li')
-  li.className='task'
-  li.textContent = tasks[i]
-  todoList.appendChild(li)
-} 
---
+++ src/main.js
@@ -0,0 +1,18 @@
+
+import { div } from "prelude-ls";
+import React from "react";
+
+
+
+const Main = () =>{
+    
+ return(  
+    <div>
+       
+    </div>
+ )
+
+
+}
+
+export default Main;
