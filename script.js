ef438bb27ec04ec0bcf5975d91f04fed5e341d32
Doolot <doolotzhaparov@gmail.com>
2022-01-31 16:35:14 +0600

    task4

---
+++ script.js
@@ -0,0 +1,12 @@
+function reverse(str){
+    const stroke = [];
+    str.split(" ").forEach(elem => {
+        if(elem.length >= 5){
+            stroke.push(elem.split("").reverse().join(""))
+        } else {
+            stroke.push(elem)
+        }
+    })
+
+    return stroke.join(" ")
+} 
